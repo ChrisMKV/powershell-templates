b4ba59611df55b1a413042526556c4203c0a17b9
ChrisMKV <41515846+ChrisMKV@users.noreply.github.com>
2021-05-13 12:59:49 +0100

    Create Get-Template.ps1

---
+++ powershell-templates/Get-Template.ps1
@@ -0,0 +1,42 @@
+function Get-Template {
+    <#
+    .SYNOPSIS
+    Describe the function here.
+    .DESCRIPTION
+    Describe the function in more detail.
+    .EXAMPLE
+    Give an example of how to use it.
+    .PARAMETER paramName
+    Describe the function parameter here.
+    .NOTES
+    Author : ChrisMKV
+    V1.0 2017-08-05 Initial Version
+	#>
+
+    #Requires -Version 5.1
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+
+    param
+    (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [string]$ExampleParameter
+    )
+
+    BEGIN {
+        Write-Debug -Message "$(Get-Date -Format s) | Call Function: $($MyInvocation.MyCommand)"
+    }
+
+    PROCESS {
+        try {
+            if ($PSCmdlet.ShouldProcess($ExampleParameter, 'WhatIsItDoing')) {
+                Write-Output -InputObject "$ExampleParameter"
+            }
+        } catch {
+            $PSCmdlet.ThrowTerminatingError($PSitem)
+        } finally {
+        }
+    }
+
+    END {
+    }
+}
