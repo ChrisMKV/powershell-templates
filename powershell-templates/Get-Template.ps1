366acfbfa909704f02fb580d071abfa17587cb37
ChrisMKV <41515846+ChrisMKV@users.noreply.github.com>
2021-05-16 12:02:39 +0200

    Add VSCode settings

--- powershell-templates/Get-Template.ps1
+++ powershell-templates/Get-Template.ps1
@@ -1,42 +1,45 @@
 function Get-Template {
-    <#
-    .SYNOPSIS
-    Describe the function here.
-    .DESCRIPTION
-    Describe the function in more detail.
-    .EXAMPLE
-    Give an example of how to use it.
-    .PARAMETER paramName
-    Describe the function parameter here.
-    .NOTES
-    Author : ChrisMKV
-    V1.0 2017-08-05 Initial Version
+	<#
+	.SYNOPSIS
+	Describe the function here.
+	.DESCRIPTION
+	Describe the function in more detail.
+	.EXAMPLE
+	Give an example of how to use it.
+	.PARAMETER paramName
+	Describe the function parameter here.
+	.NOTES
+	Author : ChrisMKV
+	V1.0 2017-08-05 Initial Version
 	#>
 
-    #Requires -Version 5.1
-    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+	#Requires -Version 5.1
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
 
-    param
-    (
-        [parameter(Mandatory, ValueFromPipeline)]
-        [string]$ExampleParameter
-    )
+	param
+	(
+		[parameter(Mandatory, ValueFromPipeline)]
+		[string]$ExampleParameter
+	)
 
-    BEGIN {
-        Write-Debug -Message "$(Get-Date -Format s) | Call Function: $($MyInvocation.MyCommand)"
-    }
+	BEGIN {
+		Write-Debug -Message "$(Get-Date -Format s) | Call Function: $($MyInvocation.MyCommand)"
+	}
 
-    PROCESS {
-        try {
-            if ($PSCmdlet.ShouldProcess($ExampleParameter, 'WhatIsItDoing')) {
-                Write-Output -InputObject "$ExampleParameter"
-            }
-        } catch {
-            $PSCmdlet.ThrowTerminatingError($PSitem)
-        } finally {
-        }
-    }
+	PROCESS {
+		try {
+			if ($PSCmdlet.ShouldProcess($ExampleParameter, 'WhatIsItDoing')) {
+				Write-Output -InputObject "$ExampleParameter"
 
-    END {
-    }
+
+
+			}
+		} catch {
+			$PSCmdlet.ThrowTerminatingError($PSitem)
+		} finally {
+		}
+	}
+
+	END {
+	}
 }
